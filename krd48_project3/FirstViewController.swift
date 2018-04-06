9f3771724861816ff31e732bf0feb1325f8f90cf
Kyle Drummond <krd48@254-12.cs.uakron.edu>
2018-04-06 14:07:55 -0400

    Initial Commit

---
+++ krd48_project3/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  krd48_project3
+//
+//  Created by Drummond,Kyle on 4/6/18.
+//  Copyright © 2018 Drummond,Kyle. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
