ec9608b8d7aa30a10f1d308f1c62deb4c9623df4
Kyle Drummond <krd48@254-12.cs.uakron.edu>
2018-04-10 12:52:12 -0400

    got writing to cloud kit started. needs more work still to read/write to/from

--- krd48_project3/FirstViewController.swift
+++ krd48_project3/FirstViewController.swift
@@ -7,18 +7,45 @@
 //
 
 import UIKit
+import CloudKit
 
 class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+     
         // Do any additional setup after loading the view, typically from a nib.
+        let publicDB = CKContainer.default().publicCloudDatabase
+        
+        // setup new mood record
+        let firstName = CKRecord(recordType: "firstName")
+        firstName["Color"] = 0 as CKRecordValue
+        
+        // save the record in a public database
+        publicDB.save(firstName) {
+            record, error in
+            
+            // NOTE: Should check error, might have a workaround
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            guard let savedRecord = record else {
+                print("Error: Unable to save record")
+                return
+            }
+            
+            print(savedRecord)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
