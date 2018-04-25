66a3f92972c0bc7253c19b19c9a5524ecb65be28
Kyle Drummond <krd48@254-12.cs.uakron.edu>
2018-04-25 14:16:13 -0400

    initial commit of adding watch kit extension

---
+++ WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WatchKit Extension
+//
+//  Created by Drummond,Kyle on 4/25/18.
+//  Copyright © 2018 Drummond,Kyle. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
